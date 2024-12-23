7d3388c4d6622efeabf665dd74f6860e9169693d
Raunak <raunakdob@gmail.com>
2024-12-24 01:06:40 +0530

    Remove commented-out weather data fetching code and update API endpoint in the service

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-// export const fetchWeatherData = async ({ latitude, longitude, startDate, endDate }) => {
-//   const API_URL = `https://api.open-meteo.com/v1/history?latitude=${latitude}&longitude=${longitude}&start_date=${startDate}&end_date=${endDate}&daily=temperature_2m_max,temperature_2m_min,temperature_2m_mean,apparent_temperature_max,apparent_temperature_min,apparent_temperature_mean&timezone=auto`;
-//   const response = await axios.get(API_URL);
-//   return response.data;
-// };
 
 class Service {
   static async fetchWeather({ latitude, longitude, startDate, endDate }) {
@@ -12,7 +7,6 @@ class Service {
     try {
       const response = await axios.get(
         `https://archive-api.open-meteo.com/v1/archive?latitude=${latitude}&longitude=${longitude}&start_date=${startDate}&end_date=${endDate}&hourly=temperature_2m`
-        // `https://api.open-meteo.com/v1/history?latitude=${latitude}&longitude=${longitude}&start_date=${startDate}&end_date=${endDate}&daily=temperature_2m_max,temperature_2m_min,temperature_2m_mean,apparent_temperature_max,apparent_temperature_min,apparent_temperature_mean&timezone=auto`
       );
       return response.data;
     } catch (error) {
